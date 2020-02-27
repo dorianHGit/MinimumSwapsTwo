bfe1c3bddc13fad9e6e9f3f3243f0aa09ca5ace0
dorianHGit <thepictueofdoriansinbox@outlook.com>
2020-02-27 13:55:36 +0000

    Functioning time efficient algorithm

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -2,6 +2,19 @@ package main;
 
 public class Solution {
     public int minSwaps(int[] arr) {
-        
+        int swaps = 0;
+        int n = arr.length;
+        for(int i = 0; i < arr.length; i++){
+            while (arr[i]-1 != i){
+                int j = arr[i]-1;
+                int tempI = arr[i];
+                int tempJ = arr[j];
+                arr[i] = tempJ;
+                arr[j] = tempI;
+                swaps++;
+            }
+        }
+        return swaps;
     }
+
 }
